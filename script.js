000fbaf653b52149fa86d9ee3a5534de91d15b9a
Karthik Meda <karthikmeda7@gmail.com>
2025-04-07 15:03:28 +0530

    JS file uploaded

---
+++ script.js
@@ -0,0 +1,22 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+var openTab = (tabname)=>{
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sideMenu");
+
+function openMenu(){
+    sidemenu.style.right = "0";
+}
+function closeMenu(){
+    sidemenu.style.right = "-200px";
+}
